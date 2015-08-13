ba4c0211ebdd03c8002dbe8cada823a4ad169190
ltjrocks77 <ltjrocks77@gmail.com>
2015-08-13 15:59:49 -0400

    Initial Commit

---
+++ DownloadConsoleGridImage/Program.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadConsoleGridImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = @"http://consolegrid.com/api";
+            var console = "Genesis";
+            var game = "Teenage Mutant Ninja Turtles - Hyperstone Heist";
+
+            var client = new RestClient(host);
+            var request = new RestRequest("top_picture?console={console}&game={game}", Method.POST);
+            request.AddUrlSegment("console", console);
+            request.AddUrlSegment("game", game);
+
+            var response = client.Execute(request);
+
+            var imageName = string.Format("{0}.png", game);
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile(response.Content, imageName);
+            }
+        }
+    }
+}
